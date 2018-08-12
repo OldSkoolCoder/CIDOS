d62efe7825d7a0f20f9a68173001b63e5083c9a5
OldSkoolCoder <JohnDale@Dalesoft.com>
2018-08-12 20:45:15 +0100

    Code for Second Video in CIDOS Development Series
    This is the start of the code for the CIDOS Development Video Series

---
+++ CIDOS.asm
@@ -0,0 +1,82 @@
+;*******************************************************************************
+;* Commodore Intergrated Disk Operation System                                 *
+;*                                                                             *
+;* Writtern By John C. Dale                                                    *
+;*                                                                             *
+;* Date 5th February 2018                                                      *
+;*******************************************************************************
+;*                                                                             *
+;* Change History                                                              *
+;* 5th Feb 2018  : Created new project for use in the new CIDOS Video Series   *
+;*******************************************************************************
+
+;*******************************************************************************
+;* Includes                                                                    *
+;*******************************************************************************
+
+ifdef TGT_C64
+    *= $8000
+endif
+
+         
+ifdef TGT_VIC20_8K
+    *= $A000
+endif
+
+incasm "libErrorCodes.asm"
+incasm "libROMRoutines.asm"
+incasm "libDOSMacros.asm"
+incasm "Character_ASCII_Const.asm"
+
+;*******************************************************************************
+;* Variables                                                                   *
+;*******************************************************************************
+
+CHRGET      = $0073
+CHRGOT      = $0079
+
+DosCommandBuffer    = $033C
+
+ifdef TGT_C64
+SCROLX  =$D016
+endif
+
+DiskCommandChannelNumber = 15
+
+;*******************************************************************************
+;* Code                                                                        *
+;*******************************************************************************
+
+
+;*******************************************************************************
+;* Show Ready Prompt                                                           *
+;*******************************************************************************
+READY
+    lda #32
+    sta 129
+    jmp bas_ReadyPrompt$       ; Ready Prompt
+
+incasm "libDOSRoutines.asm"
+
+;*******************************************************************************
+;* Storage Locations                                                           *
+;*******************************************************************************
+DEVICE_NUMBER
+    BYTE 08
+;PREVIOUS_BASIC_JUMP_VECTORS
+;    WORD 0
+;PREVIOUS_ERROR_JUMP_VECTORS
+;    WORD 0
+FILENAME
+    TEXT "0000000000000000000"
+    BRK
+;LOCK_UNLOCK_COMMAND
+;    BYTE " "                ; L = Lock, U = Unlock
+;CURRENTTRACKNO
+;    BYTE 0
+;CURRENTSECTORNO
+;    BYTE 0
+;NEXTTRACKNO
+;    BYTE 0
+;NEXTSECTORNO
+;    BYTE 0
---
+++ libDOSRoutines.asm
@@ -0,0 +1,99 @@
+;*******************************************************************************
+;* Open Disk Command Channel Routine                                           *
+;*******************************************************************************
+OpenDiskCommandChannel
+    pha                 ; Temp Store Acc.
+    tya
+    pha                 ; Temp Store Y Reg
+    txa
+    pha                 ; Temp Store X Reg
+    lda #DiskCommandChannelNumber   ; Load Logical File Number
+    ldx DEVICE_NUMBER   ; Load Current Drive Device Number
+    ldy #15             ; Load Command Type Number
+    jsr krljmp_SETLFS$  ; Set Output Device
+    lda #1              ; Load Length of Init String
+    ldx #<STARTNM       ; Load LoByte Address of Init String
+    ldy #>STARTNM       ; Load HiByte Address of Init String
+    jsr krljmp_SETNAM$  ; Set Message Parameters for Channel
+    jsr krljmp_OPEN$    ; Open File Number Coimmand Channel
+TODO Add Error Trapping Code.
+    pla                 ; Get X Reg
+    tax
+    pla                 ; Get Y Reg
+    tay
+    pla                 ; Get Acc.
+    rts
+
+;*******************************************************************************
+;* Close Disk Command Channel Routine                                          *
+;*******************************************************************************
+CloseDiskCommandChannel
+    jsr krljmp_CLRCHN$              ; Clear Channel
+    lda #DiskCommandChannelNumber   ; Load Logial File Number
+    jsr krljmp_CLOSE$               ; Close Logical File Number
+    rts
+
+STARTNM
+    text "i"
+
+;*******************************************************************************
+;* Disk Operating System Routine                                               *
+;* Inputs : Y Register is end of DOS Text                                      *
+;*******************************************************************************
+DOS
+    jsr OpenDiskCommandChannel  ; Opens Disk Command Channel
+    lda #CHR_Return             ; Load Carraige Return
+    sta DosCommandBuffer,y      ; Store it at end Of DOS Instruction Line
+
+;*************** Rem this line out once debugging is over **********************
+;    jsr ShowDOSCommand      ; Debug Information about command been sent
+
+    jsr AreYouSurePrompt    ; Confirm Operation is Required?
+    ldx #15                 ; Load Logial File Number
+    jsr krljmp_CHROUT$      ; Sets Output Device
+    lda #<DosCommandBuffer  ; Load LoByte DOS Buffer Address
+    ldy #>DosCommandBuffer  ; Load HiByte DOS Buffer Address
+    jsr bas_PrintString$    ; Print String @ Acc, Y
+    jsr krljmp_CLRCHN$      ; Clear Channel
+    lda #0                  ; Init Acc.
+    tay                     ; Init Y
+
+DOS_ClearBuffer
+    sta DosCommandBuffer,y  ; Clear Buffer
+    iny                     ; Increase Looper
+    cpy #195                ; Have we hit Byte 195 of Buffer
+    bne DOS_ClearBuffer     ; No, Clear Next byte
+    jmp ST_ChannelAlreadyOpen
+
+;*******************************************************************************
+;* Show DOS Command before Committing                                          *
+;*******************************************************************************
+ShowDOSCommand
+    lda #<DosCommandBuffer  ; Load LoByte DOS Buffer Address
+    ldy #>DosCommandBuffer  ; Load HiByte DOS Buffer Address
+    jsr bas_PrintString$    ; Print String @ Acc, Y
+    rts
+
+;*******************************************************************************
+;* Are You Sure Prompt                                                         *
+;*******************************************************************************
+AreYouSurePrompt
+    lda #CHR_Return         ; Load Carraige Return
+    jsr krljmp_CHROUT$      ; Print Character
+    lda #<PROMPT_TEXT       ; Load LoByte Address
+    ldy #>PROMPT_TEXT       ; Load HiByte Address
+    jsr bas_PrintString$    ; Print String
+    jsr krljmp_CHRIN$       ; Wait for a character response
+    cmp #"y"                ; is it 'Y'
+    bne AreYouSurePrompt1   ; No, then abort current operation
+    rts                     ; Yes, Continue with operation
+
+AreYouSurePrompt1
+    pla                     ; Pull HiByte Return Vector
+    pla                     ; Pull LoByte Return Vector
+    jmp READY               ; Goto Ready Process
+
+PROMPT_TEXT
+    TEXT "are you sure ?n"
+    BYTE CHR_CursorLeft   
+    brk 
